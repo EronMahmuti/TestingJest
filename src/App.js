bb5f9a9dd8cc7c4980f8118a8ae0240e8f666d48
Eron <64994381+EronMahmuti@users.noreply.github.com>
2023-07-02 18:26:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './components/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <ul>
+        <li>Banana </li>
+        <li>Orange </li>
+        <li>Apple </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
